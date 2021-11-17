0c77eedd4d585de35b08939cbd80659d59e395d7
sinahenning <henningsina@gmail.com>
2021-11-17 11:44:52 +0100

    #7 implemented

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "Index page", :type => :feature do
   
-  it "should display 'Hello Rails' and have link" do
+  it "should display Paper Management System and have link" do
     visit root_path
-    expect(page).to have_text("Hello, Rails!")
+    expect(page).to have_text("Paper Management System")
     expect(page).to have_link 'My Blog', href: authors_path
   end
 end
