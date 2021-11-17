8488522349e64da894ca415635c9c087d035a074
sinahenning <henningsina@gmail.com>
2021-11-17 11:41:01 +0100

    #6 implemented

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,6 +2,10 @@ class AuthorsController < ApplicationController
     def new
     end
 
+    def index
+        @authors = Author.all
+    end
+
     def show
         @author = Author.find(params[:id])
       end
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe "Index page", :type => :feature do
   
-  it "should display 'Paper Management System'" do
+  it "should display 'Hello Rails' and have link" do
     visit root_path
-    expect(page).to have_text("Paper Management System")
+    expect(page).to have_text("Hello, Rails!")
+    expect(page).to have_link 'My Blog', href: authors_path
   end
 end
